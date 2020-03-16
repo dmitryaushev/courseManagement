5a0be8060ce825d2a604b74c68b61b4a99532ccb
Aushev <dmitryaushev@gmail.com>
2020-03-16 15:12:38 +0200

    Refactor Help

--- src/main/java/com/courses/management/common/commands/Help.java
+++ src/main/java/com/courses/management/common/commands/Help.java
@@ -2,6 +2,11 @@ package com.courses.management.common.commands;
 
 import com.courses.management.common.Command;
 import com.courses.management.common.View;
+import com.courses.management.common.commands.util.InputString;
+import com.courses.management.course.CourseStatus;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class Help implements Command {
     private View view;
@@ -16,20 +21,43 @@ public class Help implements Command {
     }
 
     @Override
-    public void process() {
-        view.write("-------------------------------------------------");
-        view.write("------------------List of commands---------------");
-        view.write("    Command             |           Description          ");
-        view.write("create_course           | create a course with a title");
-        view.write("find_course_by_id       | find course by id");
-        view.write("find_course_by_title    | find course by title");
-        view.write("find_courses_by_status  | find courses by status");
-        view.write("find_all_courses        | find all courses");
-        view.write("update_course           | update course");
-        view.write("delete_course           | delete course");
-        view.write("create_user             | create a user");
-        view.write("find_user_by_email      | find user by email");
-        view.write("exit                    | exit application");
-        view.write("-------------------------------------------------");
+    public void process(InputString input) {
+        view.write("-----------------------COMMANDS--------------------------");
+        view.write("---------------------------------------------------------");
+        view.write("\t| create_course|title");
+        view.write("\t|\t-> create a course with a title");
+        view.write("---------------------------------------------------------");
+
+        view.write("\t| show_courses");
+        view.write("\t|\t-> show all courses");
+        view.write("---------------------------------------------------------");
+
+        view.write("\t| find_course|title");
+        view.write("\t|\t-> find course by title");
+        view.write("---------------------------------------------------------");
+
+        view.write("\t| update_course_title|oldTitle|newTitle");
+        view.write("\t|\t-> update course title. Title is a unique field");
+        view.write("---------------------------------------------------------");
+
+        view.write("\t| update_course_status|title|status");
+        view.write("\t|\t-> update course status. Possible status values: ");
+        view.write("\t|\t-> " + collectCourseStatuses());
+        view.write("---------------------------------------------------------");
+
+        view.write("\t| delete_course|title");
+        view.write("\t|\t-> move course to a DELETED status");
+        view.write("---------------------------------------------------------");
+
+        view.write("\t| exit");
+        view.write("\t|\t-> exit application");
+        view.write("---------------------------------------------------------");
+        view.write("---------------------------------------------------------");
+    }
+
+    private String collectCourseStatuses() {
+        return Arrays.stream(CourseStatus.values())
+                .map(CourseStatus::getStatus)
+                .collect(Collectors.joining(", "));
     }
 }
