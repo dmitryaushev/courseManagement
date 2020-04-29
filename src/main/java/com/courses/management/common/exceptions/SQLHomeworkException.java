6560e1ab4b374bd52bf3343c12fcb263b05af186
Aushev <dmitryaushev@gmail.com>
2020-04-29 23:47:29 +0300

    Remove redundant logic

--- src/main/java/com/courses/management/common/exceptions/SQLHomeworkException.java
+++
@@ -1,8 +0,0 @@
-package com.courses.management.common.exceptions;
-
-public class SQLHomeworkException extends RuntimeException{
-
-    public SQLHomeworkException(String message) {
-        super(message);
-    }
-}
